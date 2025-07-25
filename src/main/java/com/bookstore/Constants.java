bf9557e5e2b2dd78cc5a7784ba7734d8b10e6c63
HiralHirpara <hiralhirpara001@gmail.com>
2025-07-24 22:11:28 -0700

    added constants file

---
+++ src/main/java/com/bookstore/Constants.java
@@ -0,0 +1,7 @@
+public class Constants {
+
+    public static final String DATABASE = "<DATABASE>";
+    public static final String USER = "<YOUR_USERNAME>";
+    public static final String PASSWORD = "<YOUR_PASSWORD>";
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/your_database";
+}
